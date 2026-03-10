48d1c26dd8b702b169ee694a9d62486f26eeb442
Raj Jaiswal <rajj91783@gmail.com>
2026-03-10 16:56:17 +0530

    Added Chats Page

---
+++ app/messages/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function MessageLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
